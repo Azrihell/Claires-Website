7d0fd29210b0c8fd0d475fbea439bdfc28f26645
Az <azrihell.l@gmail.com>
2024-07-29 14:43:09 -0500

    removed extra links from home page

--- app/page.tsx
+++ app/page.tsx
@@ -39,10 +39,9 @@ function Home() {
       {/* Page intro text */}
       <div className="content-1140 header-content center-relative block">
         <h1 className="entry-title">
-          Art studio from <Link href="/about">New York</Link> passionate about
-          creativity and crafting things since 1963.
+          Just thinking about <Link href="/about">New Mune Faire</Link> and all the wonders of the worlds.
         </h1>
-        <p className="page-desc">PETER WALL + COCO BASIC</p>
+        <p className="page-desc">What's New and Exciting?</p>
       </div>
 
       <div id="content" className="site-content">
@@ -61,10 +60,10 @@ function Home() {
               <div className="portfolio-text-holder">
                 <div className="portfolio-info">
                   <Link className="portfolio-text" href="/single-portfolio">
-                    Clothes
+                    Trilogy
                   </Link>
                   <p className="portfolio-category">
-                    <Link href="">Fashion</Link>
+                    <Link href="">Books</Link>
                   </p>
                   <p className="portfolio-arrow">
                     <Link href="/single-portfolio">
@@ -88,10 +87,10 @@ function Home() {
               <div className="portfolio-text-holder">
                 <div className="portfolio-info">
                   <Link className="portfolio-text" href="/single-portfolio">
-                    Sweatshirt
+                    Coloring Book
                   </Link>
                   <p className="portfolio-category">
-                    <Link href="">Fashion</Link>
+                    <Link href="">Books</Link>
                   </p>
                   <p className="portfolio-arrow">
                     <Link href="/single-portfolio">
@@ -115,10 +114,10 @@ function Home() {
               <div className="portfolio-text-holder">
                 <div className="portfolio-info">
                   <Link className="portfolio-text" href="/single-portfolio">
-                    Office
+                    Amazon Kindle
                   </Link>
                   <p className="portfolio-category">
-                    <Link href="">Interior</Link>
+                    <Link href="">Read it Here</Link>
                   </p>
                   <p className="portfolio-arrow">
                     <Link href="/single-portfolio">
@@ -136,8 +135,7 @@ function Home() {
             viewport={{ once: true }}
             className="grid-item element-item quote-item "
           >
-            Anyone can get dressed up and look glamorous but it is how people
-            dress in their days off that are the most intriguing
+            Mysterious Quote from amazing book.
           </motion.li>
           <motion.li
             initial={{ opacity: 0, transform: `translateY(50px)` }}
@@ -166,210 +164,7 @@ function Home() {
               </div>
             </div>
           </motion.li>
-          <motion.li
-            initial={{ opacity: 0, transform: `translateY(50px)` }}
-            whileInView={{ opacity: 1, transform: `translateY(0px)` }}
-            viewport={{ once: true }}
-            className="grid-item element-item "
-          >
-            <div className="item-wrapper">
-              <Link href="/single-portfolio">
-                <img src={img05.src} alt="" />
-              </Link>
-              <div className="portfolio-text-holder">
-                <div className="portfolio-info">
-                  <Link className="portfolio-text" href="/single-portfolio">
-                    Notes
-                  </Link>
-                  <p className="portfolio-category">
-                    <Link href="">Branding</Link>
-                  </p>
-                  <p className="portfolio-arrow">
-                    <Link href="/single-portfolio">
-                      <img src={arrow.src} alt="" />
-                    </Link>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </motion.li>
-          <motion.li
-            initial={{ opacity: 0, transform: `translateY(50px)` }}
-            whileInView={{ opacity: 1, transform: `translateY(0px)` }}
-            viewport={{ once: true }}
-            className="grid-item element-item "
-          >
-            <div className="item-wrapper">
-              <Link href="/single-portfolio">
-                <img src={img06.src} alt="" />
-              </Link>
-              <div className="portfolio-text-holder">
-                <div className="portfolio-info">
-                  <Link className="portfolio-text" href="/single-portfolio">
-                    Building
-                  </Link>
-                  <p className="portfolio-category">
-                    <Link href="">Architecture</Link>
-                  </p>
-                  <p className="portfolio-arrow">
-                    <Link href="/single-portfolio">
-                      <img src={arrow.src} alt="" />
-                    </Link>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </motion.li>
-          <motion.li
-            initial={{ opacity: 0, transform: `translateY(50px)` }}
-            whileInView={{ opacity: 1, transform: `translateY(0px)` }}
-            viewport={{ once: true }}
-            className="grid-item element-item "
-          >
-            <div className="item-wrapper">
-              <Link href="/single-portfolio">
-                <img src={img07.src} alt="" />
-              </Link>
-              <div className="portfolio-text-holder">
-                <div className="portfolio-info">
-                  <Link className="portfolio-text" href="/single-portfolio">
-                    Desk
-                  </Link>
-                  <p className="portfolio-category">
-                    <Link href="">Interior</Link>
-                  </p>
-                  <p className="portfolio-arrow">
-                    <Link href="/single-portfolio">
-                      <img src={arrow.src} alt="" />
-                    </Link>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </motion.li>
-          {/* Items that are hidden and appears when clicking load more button */}
-          {loadedAll && (
-            <>
-              <motion.li
-                initial={{ opacity: 0, transform: `translateY(50px)` }}
-                whileInView={{ opacity: 1, transform: `translateY(0px)` }}
-                viewport={{ once: true }}
-                className="grid-item element-item"
-              >
-                <div className="item-wrapper">
-                  <Link href="/single-portfolio">
-                    <img src={img09.src} alt="" />
-                  </Link>
-                  <div className="portfolio-text-holder">
-                    <div className="portfolio-info">
-                      <Link className="portfolio-text" href="/single-portfolio">
-                        Shoes
-                      </Link>
-                      <p className="portfolio-category">
-                        <Link href="">Fashion</Link>
-                      </p>
-                      <p className="portfolio-arrow">
-                        <Link href="/single-portfolio">
-                          <img src={arrow.src} alt="" />
-                        </Link>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </motion.li>
-              <motion.li
-                initial={{ opacity: 0, transform: `translateY(50px)` }}
-                whileInView={{ opacity: 1, transform: `translateY(0px)` }}
-                viewport={{ once: true }}
-                className="grid-item element-item"
-              >
-                <div className="item-wrapper">
-                  <Link href="/single-portfolio">
-                    <img src={img11.src} alt="" />
-                  </Link>
-                  <div className="portfolio-text-holder">
-                    <div className="portfolio-info">
-                      <Link className="portfolio-text" href="/single-portfolio">
-                        Mural
-                      </Link>
-                      <p className="portfolio-category">
-                        <Link href="">Architecture</Link>
-                      </p>
-                      <p className="portfolio-arrow">
-                        <Link href="/single-portfolio">
-                          <img src={arrow.src} alt="" />
-                        </Link>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </motion.li>
-              <motion.li
-                initial={{ opacity: 0, transform: `translateY(50px)` }}
-                whileInView={{ opacity: 1, transform: `translateY(0px)` }}
-                viewport={{ once: true }}
-                className="grid-item element-item"
-              >
-                <div className="item-wrapper">
-                  <Link href="/single-portfolio">
-                    <img src={img08.src} alt="" />
-                  </Link>
-                  <div className="portfolio-text-holder">
-                    <div className="portfolio-info">
-                      <Link className="portfolio-text" href="/single-portfolio">
-                        Vase
-                      </Link>
-                      <p className="portfolio-category">
-                        <Link href="">Interior</Link>
-                      </p>
-                      <p className="portfolio-arrow">
-                        <Link href="/single-portfolio">
-                          <img src={arrow.src} alt="" />
-                        </Link>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </motion.li>
-              <motion.li
-                initial={{ opacity: 0, transform: `translateY(50px)` }}
-                whileInView={{ opacity: 1, transform: `translateY(0px)` }}
-                viewport={{ once: true }}
-                className="grid-item element-item"
-              >
-                <div className="item-wrapper">
-                  <Link href="/single-portfolio">
-                    <img src={img10.src} alt="" />
-                  </Link>
-                  <div className="portfolio-text-holder">
-                    <div className="portfolio-info">
-                      <Link className="portfolio-text" href="/single-portfolio">
-                        Birds
-                      </Link>
-                      <p className="portfolio-category">
-                        <Link href="">Branding</Link>
-                      </p>
-                      <p className="portfolio-arrow">
-                        <Link href="/single-portfolio">
-                          <img src={arrow.src} alt="" />
-                        </Link>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </motion.li>
-            </>
-          )}
-        </ul>
-        <div className="clear"></div>
-        {/* Showing more items buttons */}
-        <div className="block center-text load-more-portfolio">
-          {!loadedAll && (
-            <button className="more-posts-portfolio" onClick={handleLoadAll}>
-              Load More
-            </button>
-          )}
-        </div>
+         </ul>
         <div className="clear"></div>
       </div>
     </main>
